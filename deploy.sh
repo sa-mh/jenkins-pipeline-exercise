6448970a7b7caa92a343a1afcbede36fe1040dd0
sa-mh <62239021+sa-mh@users.noreply.github.com>
2020-10-30 14:25:53 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose 
 sudo docker-compose pull && sudo -E DB_PASSWORD=${DB_PASSWORD} docker-compose up -d
